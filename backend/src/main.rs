f32eb9a766de323416e88ea568ec5d3af928d9d4
adil <adil.business4064@gmail.com>
2026-02-18 16:52:04 +0530

    added gitignore

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -7,8 +7,6 @@ use routes::*;
 
 #[actix_web::main]
 async fn main()-> Result<()>{
-  dotenv::dotenv().ok();
-  env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
 
   HttpServer::new(move || {
     App::new()
--- backend/src/routes/user.rs
+++ backend/src/routes/user.rs
@@ -1,8 +1,8 @@
-use actix_web::{HttpRequest, HttpResponse, Responder, delete, get, post, web};
+use actix_web::{HttpResponse, Responder, delete, get, post, web};
 use crate::utils::*;
 
 #[post("/sign-up")]
-pub async fn sign_up(_req: HttpRequest)->impl Responder{
+pub async fn sign_up()->impl Responder{
   let response: Response<()> = Response{
     success: true,
     status_code: 200,
@@ -12,15 +12,15 @@ pub async fn sign_up(_req: HttpRequest)->impl Responder{
   send_response(response)
 }
 #[post("/sign-in")]
-pub async fn sign_in(_req: HttpRequest)->impl Responder{
+pub async fn sign_in()->impl Responder{
   HttpResponse::Ok().body("sign in end point")
 }
 #[get("/get-user")]
-pub async fn get_user(_req: HttpRequest)->impl Responder{
+pub async fn get_user()->impl Responder{
   HttpResponse::Ok().body("get user end point")
 }
 #[delete("/log-out")]
-pub async fn log_out(_req: HttpRequest)->impl Responder{
+pub async fn log_out()->impl Responder{
   HttpResponse::Ok().body("log out end point")
 }
 

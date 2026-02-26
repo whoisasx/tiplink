fde1e5630d21346d13377fe6b3d176aeeaac32e8
adil <adil.business4064@gmail.com>
2026-02-27 03:09:56 +0530

    swap:dto.rs file updated

--- backend/src/modules/swap/dto.rs
+++ backend/src/modules/swap/dto.rs
@@ -1,27 +1,28 @@
 use chrono::{DateTime, Utc};
 use serde::{Deserialize, Serialize};
 
-#[derive(Deserialize)]
-pub struct GetQuoteQuery{
+#[derive(Deserialize,Clone)]
+pub struct SwapQuoteQuery{
   pub input_mint: String,
-  pub output_min: String,
+  pub output_mint: String,
   pub amount: u64,
-  slippage_bps: u16
+  pub slippage_bps: u16,
 }
 
-#[derive(Serialize)]
-pub struct QuoteQueryResponse{
+#[derive(Serialize,Deserialize)]
+pub struct SwapQuoteResponse{
   pub input_mint: String,
   pub output_mint: String,
   pub input_amount: u64,
   pub output_amount: u64,
   pub price_impact: String,
   pub route_label: String,
-  pub slippage_bps: u16
+  pub slippage_bps: u16,
+  pub quote_raw: serde_json::Value,
 }
 
 #[derive(Deserialize)]
-pub struct GetSwapHistoryQuery{
+pub struct SwapHistoryQuery{
   pub page: u32,
   pub limit: u32
 }
@@ -43,7 +44,7 @@ pub enum SwapStatus{
 }
 
 #[derive(Serialize)]
-pub struct SwapHistoryResponse{
+pub struct SwapHistoryList{
   pub id: String,
   pub signature: String,
   pub stattus: SwapStatus,
@@ -54,4 +55,17 @@ pub struct SwapHistoryResponse{
   pub price_impact: String,
   pub route_label: String,
   pub created_at: DateTime<Utc>
+}
+
+#[derive(Serialize)]
+pub struct SwapHistoryResponse{
+  pub pagination: SwapPaginated,
+  pub swaps: Vec<SwapHistoryList>
+}
+
+#[derive(Serialize)]
+pub struct SwapExecuteResponse{
+  pub txn_id: String,
+  pub signature: String,
+  pub status: SwapStatus
 }
